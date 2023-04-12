1b23b52f33a5f7d97f1207a4be91e7e50a4a2caf
MarcosLemos-dev <dasilvalemosm@gmail.com>
2023-04-12 18:57:59 -0300

    adicionando comentarios sobre os codigo do servidor e cliente

--- Socket/Cliente.py
+++ Socket/Cliente.py
@@ -12,5 +12,19 @@ print("Recebemos: ", resposta)
 obj_socket.close()
 
 """
+Detalhando as linhas do código apresentado:
+
+1. Importamos as funções da biblioteca socket e criamos duas variáveis, uma para representar o servidor,
+caso o servidor não fosse a máquina local, poderíamos definir o IP do servidor externo, se fosse o caso.
+Cuidado ao definir a porta, deve ser a mesma em que inicializamos o servidor.
+2. Na quarta linha, definimos o conteúdo da variável “msg”, por meio de uma entrada do usuário (input).
+Observe que utilizamos a função “bytes()” para converter o conteúdo do input, ou seja, a string para o
+formato bytes, mais uma vez, lembrando: o socket transmite somente dados do tipo byte. A função
+“bytes()” possui o segundo parâmetro que faz referência ao padrão de caracteres “utf-8”.
+3. As três próximas linhas são referentes à criação do objeto socket (por meio da função “socket()”), à
+conexão com o servidor, por meio da função “connect()”, e, finalmente, enviando uma mensagem para o
+servidor, utilizando o método “send()”.
+4. A variável resposta recebe o dado enviado pelo servidor, limitando o tamanho para 1024 bytes.
+5. E finalizamos o código, exibindo a resposta e fechando conexão.
 
 """
--- Socket/Servidor.py
+++ Socket/Servidor.py
@@ -16,5 +16,38 @@ while True:
         break
     con.close()
     """
-    
+    Vamos a uma explicação sobre o que está ocorrendo nesse código:
+
+1. Primeiramente, importamos todas (*) as funções da biblioteca “socket”, isso será preciso para que
+possamos criar objetos do tipo “socket”.
+2. Criamos duas variáveis, “servidor” e “porta”, para armazenarmos esses dois dados que serão utilizados
+posteriormente; em “servidor”, poderíamos utilizar também a palavra “localhost” para identificarmos que
+o servidor é a própria máquina que está executando o código, o que também é representado pelo
+endereço “127.0.0.1”, que foi aqui utilizado. Em “porta”, escolhemos uma porta representada por um
+número inteiro entre 0 e 65535, em que normalmente as portas entre 0 e 1023 são portas utilizadas, por
+padrão, para atribuições de serviços conhecidos por meio do sistema operacional.
+
+3. Na quarta linha, criamos o nosso objeto socket “obj_socket”, por meio da função “socket()”, que exige
+dois parâmetros: o primeiro definirá a família responsável por identificar os pacotes. Para o nosso
+exemplo, definimos como AF_INET, o que significa que iremos utilizar a identificação do
+emissor/receptor do(s) pacote(s) via DNS ou número IP (poderíamos utilizar a constante AF_UNIX, o
+que mudaria a forma de identificar a origem/destino do(s) pacote(s)). Já o segundo parâmetro refere-se
+ao transporte desse pacote, que pode ser SOCK_STREAM, que representa o protocolo TCP (o que nós
+optamos) ou SOCK_DGRAM, que representa o protocolo de transporte UDP.
+4. Na próxima linha, fazemos a associação no nosso objeto socket com o nosso servidor e porta.
+5. Na linha em que utilizamos a função “listen()”, estamos definindo o máximo de clientes que o nosso
+servidor irá atender simultaneamente, para o nosso caso, definimos que serão, no máximo, dois (2)
+clientes.
+6. Montamos, na sequência, dois laços infinitos (enquanto for verdadeiro): no primeiro, aguardamos a
+chamada de um cliente (por meio da função accept()), assim que tivermos, receberemos uma tupla e
+iremos direcionar a identificação do cliente para a variável “cliente” e a identificação da conexão para a
+variável “con” e, então, iremos exibir a identificação do nosso cliente;
+7. No segundo laço, aguardando uma solicitação que pode ser transmitida em pacotes de 1024 bytes,
+exibimos a mensagem recebida e geramos uma mensagem para enviar no formato de “bytes” (por isso,
+a mensagem começa com “b” e, em seguida, a string que se deseja), enviamos por meio do método
+“send()” e encerramos esse segundo laço while.
+8. Finalmente, fechamos a conexão e voltamos a aguardar uma nova conexão.
+
+Todos os dados transmitidos via socket devem estar no formato byte. O socket não envia nem recebe da‐
+dos strings, por exemplo.
     """
